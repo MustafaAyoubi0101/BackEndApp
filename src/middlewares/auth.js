5c7e2faa6c906f6e9529d5b42662ffd2a4cdc5d8
Mustafa Ayoubi <mustafaayoubieo@gmail.com>
2024-01-17 05:04:17 -0800

    minor chagne

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,22 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/user');
+const debug = require('debug')("app:main");
 
 async function isLoggined(req,res,next){
   const tokenBearer = req.header("Authorization");
-  const token = tokenBearer.slice(7, );
+  const token = tokenBearer?.slice(7, );
   if(!token) res.status(401).send('access denied');
+  debug(token)
   try{
     const decoded = jwt.verify(token, config.get('jwt_key'));
     const user = await User.findById(decoded._id);
     req.user = user;
     next();
   }catch(ex){
-    res.status(400).send('invalid token');
+    debug(token)
+    await res.status(400).send('invalid token');
+    return
   }
 }
 
