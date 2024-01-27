828e014374f72634775e72927d958ee9a14576b1
Mustafa Ayoubi <mustafaayoubieo@gmail.com>
2024-01-27 20:51:42 +0430

    change made expenses

--- src/routes/expenses/controller.js
+++ src/routes/expenses/controller.js
@@ -1,65 +1,100 @@
-const controller = require("./../controller");
+const controller = require("../controller");
 const _ = require("lodash");
-const bcrypt = require("bcrypt");
-const config = require("config");
-const jwt = require("jsonwebtoken");
 const debug = require('debug')('app:main');
 
 module.exports = new (class extends controller {
 
   async getExpenses(req, res) {
-    const expenses = await this.Expenses.find({account_id: req.query.accountId});
-    res.json(expenses)
+    const page = parseInt(req.query.page) || 0;
+    const size = parseInt(req.query.size) || 10;
+    const sort = parseInt(req.query.sort) || -1;
+
+    const note = req.query.note;
+
+    const searchCondition = {
+      account_id: req.query.accountId,
+      ...(note && { note: { $regex: new RegExp(note, 'i') } }),
+    };
+
+    await this.handleAsyncOperation(
+      res,
+      async () => {
+        const [data, totalEntity] = await Promise.all([
+          this.Expenses
+            .find(searchCondition)
+            .sort({ createdAt: sort })
+            .skip((page * size))
+            .limit(size),
+          this.Expenses.countDocuments(searchCondition)
+        ]);
+
+        return {
+          data,
+          totalEntity,
+          currentPage: page,
+          totalPages: Math.ceil(totalEntity / size)
+        };
+      },
+      'Expenses fetched successfully',
+      'Error fetching Expenses'
+    );
   }
 
   async getExpense(req, res) {
-    const income = await this.Expenses.findById(req.params.id);
-    res.json(income)
+    await this.handleAsyncOperation(
+      res,
+      async () => await this.Expenses.findById(req.params.id),
+      'Expense fetched successfully',
+      'Error fetching Expense'
+    );
   }
 
   async createExpenses(req, res) {
-    const expenses = await this.Expenses(_.pick(req.body, ["account_id", "expensesAmount", "category", "paymentMethod", "notes", "date"]))
-    await expenses.save();
-
-    this.response({
+    await this.handleAsyncOperation(
       res,
-      message: "the expenses successfuly created",
-      data: _.pick(req.body, ["account_id", "expensesAmount", "category", "paymentMethod", "notes", "date"]),
-    })
+      async () => {
+        const expenses = await this.Expenses(_.pick(req.body, ["accountId", "expensesAmount", "category", "paymentMethod", "note", "date"]));
+        await expenses.save();
+        return _.pick(req.body, ["accountId", "expensesAmount", "category", "paymentMethod", "note", "date"]);
+      },
+      'Expenses created successfully',
+      'Error creating expenses'
+    );
   }
 
   async updateExpenses(req, res) {
-    const expenses = await this.Expenses.findById(req.query.id)
-    if(!expenses) {
-      this.response({res})
-      return;
-    };
-    expenses.set({
-      ...req.body,
-      expensesAmount: req.body.expensesAmount,
-      category: req.body.category,
-      notes: req.body.notes,
-      paymentMethod: req.body.paymentMethod,
-      date: req.body.date
-    })
-
-    await expenses.save();
-
-    this.response({
+    await this.handleAsyncOperation(
       res,
-      message: "the expenses successfuly updated",
-      data: _.pick(req.body, ["account_id", "expensesAmount", "category", "paymentMethod", "notes", "date"]),
-    })
+      async () => {
+        const expenses = await this.Expenses.findById(req.query.id);
+        if (!expenses) {
+          this.response({ res });
+          return;
+        }
+
+        expenses.set({
+          ...req.body,
+          expensesAmount: req.body.expensesAmount,
+          category: req.body.category,
+          note: req.body.note,
+          paymentMethod: req.body.paymentMethod,
+          date: req.body.date
+        });
+
+        await expenses.save();
+        return _.pick(req.body, ["account_id", "expensesAmount", "category", "paymentMethod", "note", "date"]);
+      },
+      'Expenses updated successfully',
+      'Error updating expenses'
+    );
   }
 
   async removeExpenses(req, res) {
-    const result = await this.Expenses.findByIdAndRemove(req.query.id);
-
-    this.response({
+    await this.handleAsyncOperation(
       res,
-      message: "the expenses successfuly removed",
-      data: result,
-    })
+      async () => await this.Expenses.findByIdAndRemove(req.query.id),
+      'Expenses removed successfully',
+      'Error removing Expenses'
+    );
   }
-
 })();
--- src/routes/expenses/validator.js
+++ src/routes/expenses/validator.js
@@ -4,22 +4,28 @@ const check = expressValidator.check;
 module.exports = new class {
   expensesValidator() {
     return [
+      check('accountId')
+        .not()
+        .isEmpty()
+        .withMessage('account ID amount is required.'),
       check('expensesAmount')
         .not()
         .isEmpty()
-        .withMessage('expenses amount is invalid'),
+        .withMessage('Expenses amount is required.')
+        .isNumeric()
+        .withMessage('Expenses amount must be a numeric value.'),
       check('category')
         .not()
         .isEmpty()
-        .withMessage('category cant be empty'),
+        .withMessage('category amount is required.'),
       check('paymentMethod')
         .not()
         .isEmpty()
-        .withMessage('Payment method cant be empty'),
+        .withMessage('Payment method amount is required.'),
       check('date')
         .not()
         .isEmpty()
-        .withMessage('date cant be empty'),
+        .withMessage('Date is required.'),
     ]
   }
 }
